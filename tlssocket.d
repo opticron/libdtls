0c326a93f84af20dd1417bc597be4c6563ccfb5d
Kinsey Moore <nyphbl8d@gmail.com>
2012-10-20 19:50:21 +0000

    Move TlsSocket into the right spot and trash clientconnection

--- tlssocket.d
+++ tlssocket.d
@@ -1,20 +1,25 @@
-module clientconnection;
+module tlssocket;
 import std.socket;
 import std.stdio;
-import gnutls;
-class TLSClientConnection {
-	Socket socket;
+import dtls.gnutls;
+/// This TcpSocket derivative acts just like a socket, except it's using TLS x509 encryption.
+/// Select probably doesn't work. Technically, it's possible to rework this to be able to use services supporting starttls.
+class TlsSocket:TcpSocket {
 	gnutls_certificate_credentials_t xcred;
 	gnutls_session_t session;
 	const int MAX_BUF = 1024;
 	char * CAFILE = "ca.pem";
-	/// Constructor requiring an InternetAddress.  This constructor sets up GNUTLS, opens the socket, and hands the socket to GNUTLS for handshaking.  When the constructor returns, the encrypted connection is open and ready for use.
-	this(InternetAddress ia) {
+	/// Constructor requiring a host and port.  This constructor creates an InternetAddress and calls the other constructor.
+	this(string host, ushort port = 25) {
+		this(new InternetAddress(host, port));
+	}
+	/// Take care of pre-connection TLS stuff
+	private void tls_pre_connect() {
 		int ret;
 		char*err;
 		gnutls_global_init ();
 		
-		/* X509 stuff */
+		// X509 stuff
 		gnutls_certificate_allocate_credentials (&xcred);
 		
 		// sets the trusted certificate authority file
@@ -23,7 +28,7 @@ class TLSClientConnection {
 		// Initialize TLS session 
 		gnutls_init (&session, GNUTLS_CLIENT);
 		
-		/* Use default priorities */
+		// Use default priorities
 		ret = gnutls_priority_set_direct (session, "PERFORMANCE", &err);
 		if (ret < 0) {
 		    if (ret == GNUTLS_E_INVALID_REQUEST) {
@@ -32,14 +37,14 @@ class TLSClientConnection {
 		    throw new Exception("TLS Error: returned with "~std.string.toString(ret));
 		}
 		
-		/* put the x509 credentials to the current session
-		 */
+		// put the x509 credentials to the current session
 		gnutls_credentials_set (session, GNUTLS_CRD_CERTIFICATE, xcred);
-		// create the connection
-		// XXX maybe fix this to use raw sockets....
-		socket = new TcpSocket(ia);
+	}
+	/// Take care of TLS handshake
+	private void tls_post_connect() {
+		int ret;
 		// lob the socket handle off to gnutls
-		gnutls_transport_set_ptr (session, cast(gnutls_transport_ptr_t) socket.handle);
+		gnutls_transport_set_ptr (session, cast(gnutls_transport_ptr_t) handle);
 
 		// Perform the TLS handshake
 		ret = gnutls_handshake (session);
@@ -48,21 +53,43 @@ class TLSClientConnection {
 		}
 		// Handshake completed
 	}
+	/// Non-autoconnecting constructor
+	override this() {
+		tls_pre_connect();
+	}
+	/// Constructor requiring an InternetAddress.
+	/// This constructor sets up GNUTLS, opens the socket, and hands the socket to GNUTLS for handshaking.
+	/// When the constructor returns, the encrypted connection is open and ready for use.
+	override this(Address ia) {
+		tls_pre_connect();
+		// create the connection
+		super(ia);
+		tls_post_connect();
+	}
+	/// Connection for non-auto-connect instantiation
+	override void connect(Address ia) {
+		super.connect(ia);
+		tls_post_connect();
+	}
 	/// Close the encrypted connection.
-	void close() {
+	override void close() {
 		gnutls_bye (session, GNUTLS_SHUT_RDWR);
-		socket.close;
+		super.close;
 		gnutls_deinit(session);
 		gnutls_certificate_free_credentials(xcred);
 		gnutls_global_deinit();
 	}
 	/// Send data over the encrypted connection.
-	void send(string toSend) {
-		gnutls_record_send (session, toSend.ptr, toSend.length);
+	override int send(void[]toSend) {
+		return gnutls_record_send (session, toSend.ptr, toSend.length);
+	}
+	/// Receive data over the encrypted connection.  This function emulates the standard socket.receive operation.
+	override int receive(void[]buffer) {
+		return gnutls_record_recv(session,cast(void*)buffer.ptr,buffer.length);
 	}
 	/// Grab data from the encrypted connection.  This operation is blocking.
 	/// Return: Any available data.  A zero-length string means the connection has been closed.
-	string receive() {
+	string getData() {
 		int ret;
   		char buffer[MAX_BUF];
 		string output = null;
@@ -83,11 +110,10 @@ class TLSClientConnection {
 	}
 }
 
-
 unittest {
-	string MSG = "GET / HTTP/1.0\r\n\r\n\0";
+	const string MSG = "GET / HTTP/1.0\r\n\r\n\0";
 	// connect to a host and start up tls
-	auto sock = new TLSClientConnection(new InternetAddress("mail.google.com",443));
+	auto sock = new TlsSocket(new InternetAddress("zimbra.digium.com",443));
 	
 	// make sure the socket gets closed and TLS turned off on exit
 	scope(exit) {
@@ -97,8 +123,9 @@ unittest {
 	// send our message!
 	sock.send(MSG);
 	// read in the response (a HTML page in this case)
-	string output = sock.receive;
+	string output = sock.getData;
 	// houston, we have html (if we've made it this far)
-	writefln("Got output from gmail: %s",output);
+	writefln("Got output from zimbra: %s",output);
 	return 0;
 }
+
